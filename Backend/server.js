6987caa209ebdb2466d7f43e6c315bf9bb030c79
Amit Bhatia <amitbhatia654@gmail.com>
2024-11-23 15:15:57 +0530

    fixes in cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,9 +5,19 @@ const { app, server } = require("./socket/socket");
 
 const AuthRoute = require('./Router/auth-router');
 const ChatRoute = require("./Router/chat-router")
+const frontendUrl = process.env.frontendUrl
 
 var cors = require('cors')
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+    origin: frontendUrl, // Allow only this origin
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    exposedHeaders: ['Authorization'], // Expose specific response headers
+    credentials: true, // Allow cookies
+    // maxAge: 600 // Cache preflight response for 10 minutes
+}));
 app.use(bodyParser.json({ limit: '50mb' }));
 
 const connectDb = require("./utils/db");
